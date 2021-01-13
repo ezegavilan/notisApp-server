b4463537165c8b162df9e511c56f46c7b3e01838
neritoo <ezegavilan95@gmail.com>
2021-01-13 13:47:10 -0300

    PostService implementado.

--- src/main/java/com/gavilan/sistemanotificacionesdemo/model/repositories/PostRepository.java
+++ src/main/java/com/gavilan/sistemanotificacionesdemo/model/repositories/PostRepository.java
@@ -1,10 +1,16 @@
 package com.gavilan.sistemanotificacionesdemo.model.repositories;
 
 import com.gavilan.sistemanotificacionesdemo.model.entities.Post;
+import com.gavilan.sistemanotificacionesdemo.model.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    List<Post> findAllByUsuario(Usuario usuario);
+
+    List<Post> findAllByOrderByCreadoEnAsc();
 }
---
+++ src/main/java/com/gavilan/sistemanotificacionesdemo/models/services/impl/PostServiceImpl.java
@@ -0,0 +1,62 @@
+package com.gavilan.sistemanotificacionesdemo.models.services.impl;
+
+import com.gavilan.sistemanotificacionesdemo.model.entities.Post;
+import com.gavilan.sistemanotificacionesdemo.model.entities.Usuario;
+import com.gavilan.sistemanotificacionesdemo.model.exceptions.NotisException;
+import com.gavilan.sistemanotificacionesdemo.model.repositories.PostRepository;
+import com.gavilan.sistemanotificacionesdemo.model.repositories.UsuarioRepository;
+import com.gavilan.sistemanotificacionesdemo.model.services.AutenticacionService;
+import com.gavilan.sistemanotificacionesdemo.model.services.PostService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final AutenticacionService autenticacionService;
+
+    @Transactional
+    @Override
+    public Post crearPost(Post post) {
+        Usuario usuarioActual = this.autenticacionService.getUsuarioActual();
+
+        Post nuevoPost = Post.builder()
+                .nombrePost(post.getNombrePost())
+                .descripcion(post.getDescripcion())
+                .creadoEn(new Date())
+                .usuario(usuarioActual).build();
+
+        return this.postRepository.save(nuevoPost);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Post> listarPosts() {
+        return this.postRepository.findAllByOrderByCreadoEnAsc();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Post obtenerPost(Long idPost) {
+        return this.postRepository.findById(idPost)
+                .orElseThrow(() -> new NotisException("Post no encontrado con id: ".concat(idPost.toString())));
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Post> obtenerPostPorUsuario(String username) {
+
+        Usuario usuario = this.usuarioRepository.findById(username)
+                .orElseThrow(() -> new NotisException("Usuario no existente con nombre de usuario: "
+                + username));
+
+        return this.postRepository.findAllByUsuario(usuario);
+    }
+}
